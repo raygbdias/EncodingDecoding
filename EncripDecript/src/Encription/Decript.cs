549c6486e66dccd9596d39a6530f4b1e75e92d05
Ray <ray.gabriel@predify.me>
2023-08-17 07:37:11 +0100

    Encoding examples
    Encoding examples has been added

---
+++ EncripDecript/src/Encription/Decript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncripDecript.src.Encription
+{
+    internal class Decript
+    {
+    }
+}
---
+++ EncripDecript/src/Encription/Encription.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace EncripDecript.src.Encription;
+
+public class Encription
+{
+    public byte[] MessageEncoder(Dictionary<string, string> input)
+    {
+        List<byte[]> blocks = new List<byte[]>();
+        return null;
+    }
+
+    Dictionary<string, string> input1 = new Dictionary<string, string>() {
+      {"sndr", "ewater"},
+      {"rcvr", "foo-works"},
+      {"sens", "heart-beat"},
+      {"time", "2023-08-16T13:07"}
+    };
+
+    Dictionary<string, string> input2 = new Dictionary<string, string>() {
+      {"sndr", "ewater"},
+      {"rcvr", "foo-works"},
+      {"sens", "temperature"},
+      {"data", "15"}
+    };
+
+    Dictionary<string, string> input3 = new Dictionary<string, string>() {
+      {"sndr", "ewater"},
+      {"rcvr", "foo-works"},
+      {"sens", "body-temperature"},
+      {"dat", "36"}
+    };
+
+}
